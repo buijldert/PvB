12c12a1f778ed7692612a50a6ed50ef3dad89297
Benjamin Uijldert <bmuijldert@gmail.com>
2018-04-12 12:44:18 +0200

    built demo further and added dotween plugin

--- Assets/_Scripts/Environment/WorldGenerator.cs
+++ Assets/_Scripts/Environment/WorldGenerator.cs
@@ -13,30 +13,31 @@ namespace Environment
 
         [SerializeField]private GameObject[] _obstaclePrefabs;
 
+        private float _spawnDelay = 1f;
+
         private void Start()
         {
-            GenerateWorld();
+            StartCoroutine(GenerateWorld());
         }
 
-        private void GenerateWorld()
+        private IEnumerator GenerateWorld()
         {
             for (int i = 0; i < _level.Length; i++)
             {
                 if(_level[i] > 0)
                 {
                     int obstacleChosen = _level[i] - 1;
-                    int oppositeObstacle = 0;
                     GameObject obstacleClone = Instantiate(_obstaclePrefabs[obstacleChosen]);
-                    if (obstacleChosen == 0)
-                        oppositeObstacle = 1;
-
-                    GameObject mirroredObstacle = Instantiate(_obstaclePrefabs[oppositeObstacle]);
-                    mirroredObstacle.transform.position = new Vector3(obstacleClone.transform.position.x-25f, obstacleClone.transform.position.y, i * 100f);
-                    obstacleClone.transform.position = new Vector3(obstacleClone.transform.position.x, obstacleClone.transform.position.y, i * 100f);
+                    GameObject mirroredObstacle = Instantiate(_obstaclePrefabs[obstacleChosen]);
+                    mirroredObstacle.transform.position = new Vector3(obstacleClone.transform.position.x-25f, obstacleClone.transform.position.y, 350f);
+                    obstacleClone.transform.position = new Vector3(obstacleClone.transform.position.x, obstacleClone.transform.position.y, 350f);
                     obstacleClone.transform.SetParent(transform);
                     mirroredObstacle.transform.SetParent(transform);
                 }
+                yield return new WaitForSeconds(_spawnDelay);
             }
+
+            StartCoroutine(GenerateWorld());
         }
     }
 }
