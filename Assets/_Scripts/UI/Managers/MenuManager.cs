af45472a526904425194a24a24be08c3013ed7c3
Tim Bajmat <t.bajmat@wearetriple.com>
2018-05-16 17:17:27 +0200

    Working Menu
    Menu is finally tweeting as intended

--- Assets/_Scripts/UI/Managers/MenuManager.cs
+++ Assets/_Scripts/UI/Managers/MenuManager.cs
@@ -3,6 +3,7 @@ using UI.Base;
 using UI.Controllers;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Linq;
 
 namespace UI.Managers
 {
@@ -19,13 +20,18 @@ namespace UI.Managers
 
         [SerializeField] private Button demoButton;
 
+        [SerializeField] private Button[] menuButtons;
+
         Sequence textFade;
 
         protected override void OnEnable()
         {
             base.OnEnable();
 
-            demoButton.onClick.AddListener(() => OnDemoButtonClicked());
+            //demoButton.onClick.AddListener(() => OnDemoButtonClicked());
+            shopButton.onClick.AddListener(() => OnShopButtonClicked());
+            homeButton.onClick.AddListener(() => OnHomeButtonClicked());
+            settingsButton.onClick.AddListener(() => OnSettingsButtonClicked());
         }
 
         protected override void Awake()
@@ -41,16 +47,16 @@ namespace UI.Managers
 
         private void Start()
         {
-            DOTextFade();
+            //DOTextFade();
+
+            DoOpeningSequence();
         }
 
         private void DOTextFade()
         {
             textFade = DOTween.Sequence();
-
             textFade.Append(pressToPlayText.GetComponent<Text>().DOFade(0, 2));
             textFade.Append(pressToPlayText.GetComponent<Text>().DOFade(1, 2));
-
 
             textFade.AppendCallback(() => 
             {
@@ -66,8 +72,6 @@ namespace UI.Managers
 
         private void DoOpeningSequence()
         {
-            Debug.Log("Help");
-
             Sequence openingSequence = DOTween.Sequence();
 
             openingSequence.Append(hexagonImage.rectTransform.DOSizeDelta(new Vector2(250, 250), 1));
@@ -96,20 +100,59 @@ namespace UI.Managers
            
         }
 
-
-
         private void OnDemoButtonClicked()
         {
             textFade.Append(pressToPlayText.DOFade(0, 2));
-
             textFade.AppendInterval(0.2f);
             textFade.Kill();
+        }
 
+        private void OnMenuButtonClicked()
+        {
+           
+        }
 
+        private void OnShopButtonClicked()
+        {
+            CheckPosition(shopButton);
+        }
 
+        private void OnHomeButtonClicked()
+        {
+            CheckPosition(homeButton);
+        }
 
+        private void OnSettingsButtonClicked()
+        {
+            CheckPosition(settingsButton);
+        }
 
+        private void CheckPosition(Button button)
+        {
+            if(button.GetComponent<RectTransform>().anchoredPosition != new Vector2(0, -650))
+            {
+                RectTransform rect = button.GetComponent<RectTransform>();
+                Vector2 vec2 = new Vector2(rect.anchoredPosition.x * -1, rect.anchoredPosition.y);
+                                          
+                Sequence s = DOTween.Sequence();
+                s.Append(button.GetComponent<RectTransform>().DOAnchorPos(new Vector2(0, -650), 1));
 
+                foreach (Button btn in menuButtons.Where(btn => btn.GetComponent<RectTransform>().anchoredPosition == new Vector2(0, -650)))
+                {
+                    s.Join(btn.GetComponent<RectTransform>().DOAnchorPos(vec2, 1));
+                }
+
+                foreach (Button btn in menuButtons.Where(btn => btn.GetComponent<RectTransform>().anchoredPosition != new Vector2(0, -650)))
+                {
+                    if(btn != button)
+                    {
+                        s.Join(btn.GetComponent<Image>().DOFade(0, 1));
+                        s.Append(btn.GetComponent<RectTransform>().DOAnchorPos(new Vector2(Mathf.CeilToInt(vec2.x * -1), vec2.y), 0.1f));
+                        s.Append(btn.GetComponent<Image>().DOFade(1, 1));
+                    }
+                        
+                }
+            }
         }
 
         protected override void StopScreen()
