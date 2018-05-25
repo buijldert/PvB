126dafceee3e2e5f9279c8aa2b58644f251049d0
Benjamin Uijldert <bmuijldert@gmail.com>
2018-05-25 17:02:34 +0200

    made profile switcher for post processing and started on volume system

---
+++ Assets/_Scripts/Utility/PostProcessingManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+namespace Utility
+{
+    /// <summary>
+    /// This class manages the postprocessing profiles for differing platforms.
+    /// </summary>
+    [RequireComponent(typeof(PostProcessingBehaviour))]
+    [ExecuteInEditMode]
+    public class PostProcessingManager : MonoBehaviour
+    {
+        [Header("Post Processing Profiles")]
+        [SerializeField] private PostProcessingProfile androidProfile;
+        [SerializeField] private PostProcessingProfile iphoneProfile;
+        [SerializeField] private PostProcessingProfile webglProfile;
+
+        private PostProcessingBehaviour postProcessingBehaviour;
+        
+        private void Awake()
+        {
+            postProcessingBehaviour = GetComponent<PostProcessingBehaviour>();
+            SelectProfile();
+        }
+
+        private void SelectProfile()
+        {
+#if UNITY_EDITOR
+            switch (EditorUserBuildSettings.activeBuildTarget)
+            {
+                case BuildTarget.Android:
+                    postProcessingBehaviour.profile = androidProfile;
+                    break;
+                case BuildTarget.iOS:
+                    postProcessingBehaviour.profile = iphoneProfile;
+                    break;
+                case BuildTarget.WebGL:
+                    postProcessingBehaviour.profile = webglProfile;
+                    break;
+            }
+#else
+            switch (Application.platform)
+            {
+                case RuntimePlatform.Android:
+                    postProcessingBehaviour.profile = androidProfile;
+                    break;
+                case RuntimePlatform.IPhonePlayer:
+                    postProcessingBehaviour.profile = iphoneProfile;
+                    break;
+                case RuntimePlatform.WebGLPlayer:
+                    postProcessingBehaviour.profile = webglProfile;
+                    break;
+            }
+#endif
+        }
+    }
+}
---
+++ Assets/_Scripts/Utility/VolumeManager.cs
@@ -0,0 +1,29 @@
+﻿using Audio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utility
+{
+    public class VolumeManager : MonoBehaviour
+    {
+        [Header("Sliders")]
+        [SerializeField] private Slider masterVolumeSlider;
+        [SerializeField] private Slider sfxVolumeSlider;
+        [SerializeField] private Slider musicVolumeSlider;
+
+        [Header("Volumes")]
+        [Range(0, 100)]
+        [SerializeField] private int masterVolume;
+        [Range(0, 100)]
+        [SerializeField] private int sfxVolume;
+        [Range(0, 100)]
+        [SerializeField] private int musicVolume;
+
+        public void ChangeMasterVolume()
+        {
+            SFXManager.instance.ChangeGroupVolume("MasterVolume", SoundHelper.VolumeToDecibel(masterVolumeSlider.value));
+        }
+    }
+}
