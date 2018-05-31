7618742e2c35651f2661bdf5992e59292fe76296
Benjamin Uijldert <bmuijldert@gmail.com>
2018-05-31 13:23:05 +0200

    made an outfit switcher

---
+++ Assets/_Scripts/Player/PlayerOutfit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    /// <summary>
+    /// This class is responible for switching the players outfit to the selected one.
+    /// </summary>
+    public class PlayerOutfit : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer blackPlayerMeshRenderer;
+        [SerializeField] private MeshRenderer whitePlayerMeshRenderer;
+
+        /// <summary>
+        /// Switches the players outfit material to the given material.
+        /// </summary>
+        /// <param name="outfitMaterial">The given outfit material.</param>
+        private void SwitchOutfit(Material outfitMaterial)
+        {
+            blackPlayerMeshRenderer.sharedMaterials[1] = outfitMaterial;
+            whitePlayerMeshRenderer.sharedMaterials[1] = outfitMaterial;
+        }
+    }
+}
