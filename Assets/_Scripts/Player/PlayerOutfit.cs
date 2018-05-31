8b6193f921c216cf9ba314f91211605fdca26559
Benjamin Uijldert <bmuijldert@gmail.com>
2018-05-31 13:42:51 +0200

    refined outfit switching a bit more

--- Assets/_Scripts/Player/PlayerOutfit.cs
+++ Assets/_Scripts/Player/PlayerOutfit.cs
@@ -9,8 +9,7 @@ namespace Player
     /// </summary>
     public class PlayerOutfit : MonoBehaviour
     {
-        [SerializeField] private MeshRenderer blackPlayerMeshRenderer;
-        [SerializeField] private MeshRenderer whitePlayerMeshRenderer;
+        [SerializeField] private MeshRenderer[] playerMeshRenderers;
 
         /// <summary>
         /// Switches the players outfit material to the given material.
@@ -18,13 +17,12 @@ namespace Player
         /// <param name="outfitMaterial">The given outfit material.</param>
         public void SwitchOutfit(Material outfitMaterial)
         {
-            Material[] blackMaterials = blackPlayerMeshRenderer.materials;
-            blackMaterials[1] = outfitMaterial;
-            blackPlayerMeshRenderer.materials = blackMaterials;
-
-            Material[] whiteMaterials = whitePlayerMeshRenderer.materials;
-            whiteMaterials[1] = outfitMaterial;
-            whitePlayerMeshRenderer.materials = whiteMaterials;
+            for (int i = 0; i < playerMeshRenderers.Length; i++)
+            {
+                Material[] rendererMaterials = playerMeshRenderers[i].materials;
+                rendererMaterials[1] = outfitMaterial;
+                playerMeshRenderers[i].materials = rendererMaterials;
+            }
         }
     }
 }
